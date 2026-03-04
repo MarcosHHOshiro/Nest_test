0c869d6699ee32237e7208904e7022e193b985af
marcos <marcos.2h.oshiro@gmail.com>
2026-03-04 14:07:22 -0400

    health check

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
+import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //versionamento da API
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  //swagger
+  const config = new DocumentBuilder()
+    .setTitle('Curso NestJS - Tasks API')
+    .setDescription('The tasks API description')
+    .setVersion('1.0')
+    .addTag('tasks')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
