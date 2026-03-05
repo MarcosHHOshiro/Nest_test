034be36bb1907aedc0059e25d71b61d9cfb7fb20
Marcos <marcos.2h.oshiro@gmail.com>
2026-03-04 20:14:03 -0400

    implementing ProjectListItemDTO

--- src/modules/projects/projects.dto.ts
+++ src/modules/projects/projects.dto.ts
@@ -10,3 +10,16 @@ export class ProjectRequestDTO {
   @IsString()
   description: string;
 }
+
+export class ProjectListItemDTO {
+  @ApiProperty({ description: 'The unique identifier of the project' })
+  id: string;
+  @ApiProperty({ description: 'The name of the project' })
+  name: string;
+  @ApiProperty({ description: 'The description of the project', required: false })
+  description: string;
+  @ApiProperty({ description: 'The date and time when the project was created' })
+  createdAt: Date;
+  @ApiProperty({ description: 'The date and time when the project was last updated' })
+  updatedAt: Date;
+}
