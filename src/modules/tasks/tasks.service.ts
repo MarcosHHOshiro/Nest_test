0bac3ec4641f5d4377e62e4b1fb81e36dd002013
marcos <marcos.2h.oshiro@gmail.com>
2026-03-06 07:27:04 -0400

    taskService

---
+++ src/modules/tasks/tasks.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+
+@Injectable()
+export class TasksService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findAllByProjectId(projectId: string) {
+    return this.prisma.task.findMany({
+      where: {
+        projectId,
+      },
+    });
+  }
+
+  async findById(projectId: string, id: string) {
+    return this.prisma.task.findUnique({
+      where: {
+        projectId,
+        id,
+      },
+    });
+  }
+
+  async create(projectId: string, data: any) {
+    return this.prisma.task.create({
+      data: {
+        ...data,
+        projectId,
+      },
+    });
+  }
+
+  async update(projectId: string, id: string, data: any) {
+    return this.prisma.task.update({
+      where: {
+        projectId,
+        id,
+      },
+      data,
+    });
+  }
+
+  async delete(projectId: string, id: string) {
+    return this.prisma.task.delete({
+      where: {
+        projectId,
+        id,
+      },
+    });
+  }
+}
